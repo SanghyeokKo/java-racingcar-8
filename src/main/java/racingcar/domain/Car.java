3ec7ac00d7c66aa8ad60677dbd6a0e6062080daf
sang <sang3141592@gmail.com>
2025-10-26 17:19:00 +0900

    feat(domain): car생성자 생성 및 전진 기능 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,4 +1,20 @@
 package racingcar.domain;
 //자동차 1대의 상태와 행위
 public class Car {
+    private String name;
+    private int position;
+    public Car(String name){
+        this.name = name;
+        this.position = 0;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public int getPosition(){
+        return this.position;
+    }
+    public void move(int number){
+        if(number > 4)
+            this.position++;
+    }
 }

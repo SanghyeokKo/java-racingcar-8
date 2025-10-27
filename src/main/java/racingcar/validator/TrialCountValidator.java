7fb720274afd746056973cea1d30c597b0ec974c
sang <sang3141592@gmail.com>
2025-10-27 21:23:11 +0900

    feat(validator): 시도 횟수 유효성 검증 기능 구현

--- src/main/java/racingcar/validator/TrialCountValidator.java
+++ src/main/java/racingcar/validator/TrialCountValidator.java
@@ -1,4 +1,21 @@
 package racingcar.validator;
 //시도 횟수 유효성 검증
 public class TrialCountValidator {
+    public static void countValidator(String input){
+        int number = parseNumber(input);
+        validateIsPositive(number);
+    }
+    private static int parseNumber(String input){
+        try{
+            return Integer.parseInt(input);
+        }catch(NumberFormatException e){
+            throw new IllegalArgumentException("시도 횟수는 숫자여야 합니다.");
+        }
+    }
+    private  static void validateIsPositive(int num){
+        if(num <= 0){
+            throw new IllegalArgumentException("시도 횟수는 0보다 커야 합니다.");
+        }
+    }
+
 }

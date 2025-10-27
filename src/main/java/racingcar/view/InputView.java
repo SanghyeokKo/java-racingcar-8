955e31eb93705b27a72d2ae4938c1e057635cef0
sang <sang3141592@gmail.com>
2025-10-27 21:44:39 +0900

    feat(view): 사용자 입력 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,14 @@
 package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
 //사용자 입력 담당
 public class InputView {
+    public static String readCarNames(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+    public static String readTrialCount(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Console.readLine();
+    }
 }

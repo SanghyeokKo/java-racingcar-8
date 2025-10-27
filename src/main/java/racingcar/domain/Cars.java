89ad98e6c15b2c43b6b8c8faeeb3d7d9bcbb9ed3
sang <sang3141592@gmail.com>
2025-10-27 12:58:11 +0900

    feat(domain): Cars 생성자 및 moveAll 기능 구현

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,4 +1,22 @@
 package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
 //자동차N대의 상태와 행위
 public class Cars {
+    private final List<Car> cars;
+    public Cars(List<String> carNames){
+        this.cars = new ArrayList<>();
+        for(String name : carNames){
+            Car newcar = new Car(name);
+            this.cars.add(newcar);
+        }
+    }
+    public void moveAll(){
+        for(Car car : cars){
+            int randomNumber = Randoms.pickNumberInRange(0,9);
+            car.move(randomNumber);
+        }
+    }
 }

6b7d71ed35e48577312617e03bc4d6fba63c1eb3
sang <sang3141592@gmail.com>
2025-10-27 16:33:25 +0900

    feat(domain): Cars 객체리스트 반환 기능 및 우승자 검색 기능 추가

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -3,6 +3,8 @@ package racingcar.domain;
 import java.util.ArrayList;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
+import org.assertj.core.api.AbstractAtomicReferenceAssert;
+
 //자동차N대의 상태와 행위
 public class Cars {
     private final List<Car> cars;
@@ -18,5 +20,25 @@ public class Cars {
             int randomNumber = Randoms.pickNumberInRange(0,9);
             car.move(randomNumber);
         }
+    }
+    public List<Car> getCars(){
+        return cars;
+    }
+    private int findMaxPosition(){
+        int maxPosition = 0;
+        for(Car car : cars){
+            maxPosition = Math.max(maxPosition, car.getPosition());
+        }
+        return maxPosition;
+    }
+    public List<String> findWinners(){
+        int maxPosition = findMaxPosition();
+        List<String> winnerNames = new ArrayList<>();
+        for(Car car : cars){
+            if(car.getPosition() == maxPosition){
+                winnerNames.add(car.getName());
+            }
+        }
+        return winnerNames;
     }
 }
